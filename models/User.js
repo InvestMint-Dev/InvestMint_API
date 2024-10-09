6117c0f646d1a17e83d2add2891bb65858557131
Leanne <sooyeunleanne@gmail.com>
2024-10-09 18:42:34 -0400

    changing userSchema again

--- models/User.js
+++ models/User.js
@@ -1,10 +1,20 @@
+// models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  auth0_id: { type: String, required: true },
-  email: { type: String, required: true },
-  name: { type: String, required: true },
-  isAuthenticated: { type: Boolean, default: false },
+  auth0_id: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // Store hashed password
+  name: { type: String },
+});
+
+// Hash password before saving user
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
