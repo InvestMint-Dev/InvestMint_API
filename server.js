04bb32ba44efa45cbf999e69527bb2116934af2d
Leanne <sooyeunleanne@gmail.com>
2024-10-09 18:40:36 -0400

    setting up server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 require('dotenv').config();
 
 // Initialize app
@@ -8,14 +11,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://<YOUR-AUTH0-DOMAIN>/.well-known/jwks.json`,
+    }),
+    audience: '<YOUR-API-AUDIENCE>',
+    issuer: `https://<YOUR-AUTH0-DOMAIN>/`,
+    algorithms: ['RS256'],
+  });
+  
+  
+// Apply the middleware globally for all routes under '/api'
+app.use('/api', checkJwt);
 
-// API Routes
-app.use('/api/users', require('./routes/userRoutes'));
-
+// Example protected route
+app.get('/api/protected', (req, res) => {
+  res.send('You are authorized to access this protected route.');
+});
 // Start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
