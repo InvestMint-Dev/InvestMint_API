8af9ba058e583945c420471500f23ea4ae010cb3
Leanne <sooyeunleanne@gmail.com>
2024-10-15 19:48:34 -0400

    modelling company information

---
+++ models/CompanyInformation.js
@@ -0,0 +1,21 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const companyInformationSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: String, required: true }, 
+    mobileNumber: { type: String, required: true }, 
+    companyName: { type: String, required: true }, 
+    companyPhoneNumber: { type: String, required: true }, 
+    companyAddressLine: { type: String, required: true }, 
+    city: { type: String, required: true }, 
+    state: { type: String, required: true }, 
+    zipcode: { type: String, required: true }, 
+    country: { type: String, required: true }, 
+    companyBankAccounts: { type: Array, required: true }, 
+    advisorName: { type: String }, 
+    companyInvestmentAccountNumber: { type: String }, 
+  });
